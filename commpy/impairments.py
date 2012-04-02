e643c61af8506ee119decca8f937881d07a5f990
Veeresh Taranalli <veeresht@gmail.com>
2012-04-01 21:20:30 -0700

    Adding impariments module

---
+++ commpy/impairments.py
@@ -0,0 +1,25 @@
+
+#   Copyright 2012 Veeresh Taranalli <veeresht@gmail.com>
+#
+#   This file is part of CommPy.   
+#
+#   CommPy is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   CommPy is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from numpy import exp, pi, arange
+
+__all__ = ['add_frequency_offset']
+
+def add_frequency_offset(waveform, Fs, delta_f):
+    
+    return waveform*exp(1j*2*pi*(delta_f/Fs)*arange(len(waveform)))
