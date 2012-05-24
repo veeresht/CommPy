6ece8f9e5f6f19ed976b1b41b9ea192739471df8
Veeresh Taranalli <veeresht@gmail.com>
2012-05-24 00:14:34 -0700

    Adding to sys.path

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import sphinx
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../commpy'))
 sys.path.insert(0, os.path.abspath('sphinxext'))
 # -- General configuration -----------------------------------------------------
 
