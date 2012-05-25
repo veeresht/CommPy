1809f33476386ea1c3afe11e00d13139d7c2f004
Veeresh Taranalli <veeresht@gmail.com>
2012-05-24 17:57:47 -0700

    Adding matplotlib.collections package to the mock code in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'cython', 'matplotlib.pyplot']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'cython', 'matplotlib.pyplot', 'matplotlib.collections']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
