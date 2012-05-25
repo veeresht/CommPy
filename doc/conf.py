ef9270ec155ffba503e52b27492c66d6cadc352a
Veeresh Taranalli <veeresht@gmail.com>
2012-05-24 17:55:55 -0700

    Adding matplotlib.pyplot package to the mock code in conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -31,7 +31,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'cython']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'cython', 'matplotlib.pyplot']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
