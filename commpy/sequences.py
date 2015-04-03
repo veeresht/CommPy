0f7e06a7618dd7d0e0cfe7c68aa08f9963154ae4
Veeresh Taranalli <veeresht@gmail.com>
2015-04-03 14:07:41 -0700

    Fixed ZC sequence generation to use the root index correctly.

--- commpy/sequences.py
+++ commpy/sequences.py
@@ -1,7 +1,7 @@
 
 #   Copyright 2012 Veeresh Taranalli <veeresht@gmail.com>
 #
-#   This file is part of CommPy.   
+#   This file is part of CommPy.
 #
 #   CommPy is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -16,14 +16,14 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""  
+"""
 ==================================================
 Sequences (:mod:`commpy.sequences`)
 ==================================================
 
 .. autosummary::
    :toctree: generated/
-    
+
    pnsequence             -- PN Sequence Generator.
    zcsequence             -- Zadoff-Chu (ZC) Sequence Generator.
 
@@ -33,16 +33,16 @@ __all__ = ['pnsequence', 'zcsequence']
 from numpy import array, empty, zeros, roll, exp, pi, arange
 
 def pnsequence(pn_order, pn_seed, pn_mask, seq_length):
-    """ Generate a PN (Pseudo-Noise) sequence using a 
-        Linear Feedback Shift Register (LFSR). 
-    
+    """ Generate a PN (Pseudo-Noise) sequence using a
+        Linear Feedback Shift Register (LFSR).
+
     Parameters
     ----------
     pn_order : int
         Number of delay elements used in the LFSR.
-    
+
     pn_seed : string containing 0's and 1's
-        Seed for the initialization of the LFSR delay elements. 
+        Seed for the initialization of the LFSR delay elements.
         The length of this string must be equal to 'pn_order'.
 
     pn_mask : string containing 0's and 1's
@@ -51,7 +51,7 @@ def pnsequence(pn_order, pn_seed, pn_mask, seq_length):
 
     seq_length : int
         Length of the PN sequence to be generated. Usually (2^pn_order - 1)
-         
+
     Returns
     -------
     pnseq : 1D ndarray of ints
@@ -59,12 +59,12 @@ def pnsequence(pn_order, pn_seed, pn_mask, seq_length):
 
     """
     # Check if pn_order is equal to the length of the strings 'pn_seed' and 'pn_mask'
-    
+
     pnseq = zeros(seq_length)
-    
+
     # Initialize shift register with the pn_seed
     sr = array(map(lambda i: int(pn_seed[i]), xrange(0, len(pn_seed))))
-    
+
     for i in xrange(seq_length):
         new_bit = 0
         for j in xrange(pn_order):
@@ -92,6 +92,6 @@ def zcsequence(u, seq_length):
     zcseq : 1D ndarray of complex floats
         ZC sequence generated.
     """
-    zcseq = exp((-1j * pi * arange(seq_length) * (arange(seq_length)+1)) / seq_length)
+    zcseq = exp((-1j * pi * u * arange(seq_length) * (arange(seq_length)+1)) / seq_length)
 
     return zcseq
